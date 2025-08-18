807f355f8f84dedc160759a2fdfcdbb029e5ba9a
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-08-18 12:55:40 +0800

    Add playGame

--- script.js
+++ script.js
@@ -16,24 +16,44 @@ function getHumanChoice() {
 let computerScore = 0;
 let humanScore = 0;
 
-function playRound(computerChoice, humanChoice) {
-  humanChoice = humanChoice.toLowerCase();
-  if (computerChoice === humanChoice) {
-    console.log('You draw!');
-  } else if (
-    (computerChoice === 'rock' && humanChoice === 'scissors') ||
-    (computerChoice === 'paper' && humanChoice === 'rock') ||
-    (computerChoice === 'scissors' && humanChoice === 'paper')
-  ) {
-    console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
-    computerScore++;
-  } else {
-    console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
-    humanScore++;
+function playGame() {
+  function playRound(computerChoice, humanChoice, roundNumber) {
+    humanChoice = humanChoice.toLowerCase();
+    let string = `Round ${roundNumber}: `;
+    if (computerChoice === humanChoice) {
+      string += 'You draw!';
+    } else if (
+      (computerChoice === 'rock' && humanChoice === 'scissors') ||
+      (computerChoice === 'paper' && humanChoice === 'rock') ||
+      (computerChoice === 'scissors' && humanChoice === 'paper')
+    ) {
+      string += `You lose, ${computerChoice} beats ${humanChoice}!`;
+      computerScore++;
+    } else {
+      string += `You win, ${humanChoice} beats ${computerChoice}!`;
+      humanScore++;
+    }
+
+    console.log(
+      `${string}
+Computer score: ${computerScore}
+Your score: ${humanScore}`
+    );
   }
 
-  console.log(`Computer score: ${computerScore}`);
-  console.log(`Your score: ${humanScore}`);
+  for (let i = 1; i < 6; i++) {
+    playRound(getComputerChoice(), getHumanChoice(), i);
+
+    if (i === 5) {
+      if (computerScore === humanScore) {
+        console.log('You draw overall!');
+      } else if (computerScore > humanScore) {
+        console.log('Computer wins overall!');
+      } else {
+        console.log('You win overall!');
+      }
+    }
+  }
 }
 
-playRound(getComputerChoice(), getHumanChoice());
+playGame();
