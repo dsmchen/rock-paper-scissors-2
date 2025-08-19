9fc780faad5f02d5882e15d2e2007620d39056ff
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-08-19 11:33:10 +0800

    Add button event listeners

--- script.js
+++ script.js
@@ -9,51 +9,39 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  return prompt('Enter rock, paper or scissors');
-}
-
 let computerScore = 0;
 let humanScore = 0;
 
-function playGame() {
-  function playRound(computerChoice, humanChoice, roundNumber) {
-    humanChoice = humanChoice.toLowerCase();
-    let string = `Round ${roundNumber}: `;
-    if (computerChoice === humanChoice) {
-      string += 'You draw!';
-    } else if (
-      (computerChoice === 'rock' && humanChoice === 'scissors') ||
-      (computerChoice === 'paper' && humanChoice === 'rock') ||
-      (computerChoice === 'scissors' && humanChoice === 'paper')
-    ) {
-      string += `You lose, ${computerChoice} beats ${humanChoice}!`;
-      computerScore++;
-    } else {
-      string += `You win, ${humanChoice} beats ${computerChoice}!`;
-      humanScore++;
-    }
+function playRound(computerChoice, humanChoice) {
+  let string = '';
 
-    console.log(
-      `${string}
+  if (computerChoice === humanChoice) {
+    string += 'You draw!';
+  } else if (
+    (computerChoice === 'rock' && humanChoice === 'scissors') ||
+    (computerChoice === 'paper' && humanChoice === 'rock') ||
+    (computerChoice === 'scissors' && humanChoice === 'paper')
+  ) {
+    string += `You lose, ${computerChoice} beats ${humanChoice}!`;
+    computerScore++;
+  } else {
+    string += `You win, ${humanChoice} beats ${computerChoice}!`;
+    humanScore++;
+  }
+
+  console.log(
+    `${string}
 Computer score: ${computerScore}
 Your score: ${humanScore}`
-    );
-  }
-
-  for (let i = 1; i < 6; i++) {
-    playRound(getComputerChoice(), getHumanChoice(), i);
-
-    if (i === 5) {
-      if (computerScore === humanScore) {
-        console.log('You draw overall!');
-      } else if (computerScore > humanScore) {
-        console.log('Computer wins overall!');
-      } else {
-        console.log('You win overall!');
-      }
-    }
-  }
+  );
 }
 
-playGame();
+const handleClick = (e) => {
+  let humanChoice = e.target.id;
+  playRound(getComputerChoice(), humanChoice);
+};
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', handleClick);
+});
